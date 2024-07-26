f44262a3bb559d4eab41f11f2682e4bfb3d3e5a1
remiltan123 <regiremiltan2002@gmail.com>
2024-07-25 20:10:37 -0700

    Get product from backend 07/26

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,18 +1,46 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
-import all_product from "../Components/Assets/all_product";
+//import all_product from "../Components/Assets/all_product";
 
 export const ShopContext = createContext(null);
 
 const getCart = () => {
   let cart = {};
-  for (let index = 0; index < all_product.length; index++) {
+  for (let index = 0; index < 300; index++) {
     cart[index] = 0; 
   }
   return cart;
 }
 
 const ShopContextProvider = (props) => {
+
+  const[all_product,Setall_products]=useState([]);
   const [cartItems, setCart] = useState(getCart());
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:4000/allproduct", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json"
+          }
+        });
+  
+        // Check if the response is okay (status code 200-299)
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+  
+        const data = await response.json();
+        Setall_products(data);
+      } catch (error) {
+        console.error("Error fetching the data:", error);
+        alert("Please wait, something went wrong");
+      }
+    };
+  
+    fetchData();
+  }, []);
 
   const Addcart = (itemId) => {
     setCart((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
