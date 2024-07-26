87486fed2a39d704b771fa892522bf6bce35ce7b
remiltan123 <regiremiltan2002@gmail.com>
2024-07-25 20:21:36 -0700

    Get product from backend 07/26

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -22,7 +22,7 @@ const ShopContextProvider = (props) => {
         const response = await fetch("http://localhost:4000/allproduct", {
           method: "GET",
           headers: {
-            "Content-Type": "application/json"
+            "ContentType": "application/json"
           }
         });
   
